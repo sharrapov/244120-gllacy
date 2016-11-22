b0a1f72edb9d7789f3348a1b1add9821b52b5fd6
sharrapov <sharapov.design@gmail.com>
2016-11-23 00:11:47 +0300

    Исправление верстки

---
+++ js/script.js
@@ -0,0 +1,65 @@
+		var link = document.querySelector(".login");
+		var popup = document.querySelector(".user-block-login");
+		var body = document.querySelector("body");
+		var search = document.querySelector(".search-link");
+		var searchModal = document.querySelector(".search-form");
+		var feedbackButton = document.querySelector(".feedback-button");
+		var feedbackModal = document.querySelector(".feedback-modal-background");
+		var close = document.querySelector(".feedback-modal-close");
+		var cart = document.querySelector(".cart-link");
+		var cartModal = document.querySelector(".cart-modal");
+
+		link.addEventListener("mouseover", function(event){
+			event.preventDefault();
+			popup.classList.add("user-block-login-show");
+		})
+		
+		popup.addEventListener("mouseover", function(event){
+			event.preventDefault();
+			popup.classList.add("user-block-login-show");
+		})
+		
+		popup.addEventListener("mouseout", function(event){
+			event.preventDefault();
+			popup.classList.remove("user-block-login-show");
+		})
+		
+		search.addEventListener("mouseover", function(event){
+			event.preventDefault();
+			searchModal.classList.add("search-form-show");
+		})
+		
+		searchModal.addEventListener("mouseover", function(event){
+			event.preventDefault();
+			searchModal.classList.add("search-form-show");
+		})
+		
+		searchModal.addEventListener("mouseout", function(event){
+			event.preventDefault();
+			searchModal.classList.remove("search-form-show");
+		})
+		
+		feedbackButton.addEventListener("click", function(event){
+			event.preventDefault();
+			feedbackModal.classList.add("feedback-modal-background-show");
+		})
+		
+		close.addEventListener("click", function(event){
+			event.preventDefault();
+			feedbackModal.classList.remove("feedback-modal-background-show");
+		})
+		
+		cart.addEventListener("mouseover", function(event){
+			event.preventDefault();
+			cartModal.classList.add("cart-modal-show");
+		})
+		
+		cartModal.addEventListener("mouseover", function(event){
+			event.preventDefault();
+			cartModal.classList.add("cart-modal-show");
+		})
+		
+		cartModal.addEventListener("mouseout", function(event){
+			event.preventDefault();
+			cartModal.classList.remove("cart-modal-show");
+		})
